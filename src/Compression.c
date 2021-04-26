ff47638a25e182227b9dcf4c888e61331913c37c
Joshua Gonzalez <joshgonzalez246@gmail.com>
2021-04-26 15:28:51 +1000

    Setup

--- src/Compression.c
+++ src/Compression.c
@@ -1,1 +1,27 @@
-Hello
+/*##############################################################
+Define:
+
+Inputs:
+- information from a text file
+Outputs:
+- new compressed text file
+##############################################################*/
+void compress()
+{
+
+
+}
+
+/*##############################################################
+Define:
+
+Inputs:
+-  new compressed text file
+Outputs:
+- uncompressed file
+##############################################################*/
+void decompress()
+{
+	
+
+}
