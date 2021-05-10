700dbe6b1d4f979c1852c634b6c8935d15f420bf
Peter DV <55121683+OrigamiDev-Pete@users.noreply.github.com>
2021-05-10 15:51:57 +1000

    Started IO

--- src/io.h
+++ src/io.h
@@ -2,12 +2,18 @@
 * Author: Peter de Vroom
 * 
 * The header file for the In/Out functionality of the program.
-*
 *******************************************************************************/
 
 #ifndef IO_H
 #define IO_H
 
-void readLine(char *str);
+#include "util.h"
+
+enum compression { huffman, runLength };
+
+bool saveData(boolean encrypt, boolean compress, enum compression cmp_type);
+
+bool loadData(boolean decrypt, boolean decompress, enum compresion cmp_type);
+
 
 #endif /* IO_H */
