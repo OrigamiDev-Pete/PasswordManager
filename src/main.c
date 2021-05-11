bb353879bd8258b8ec4d99f6b7e970b2aada0dc5
Peter de Vroom <55121683+OrigamiDev-Pete@users.noreply.github.com>
2021-05-11 10:57:21 +1000

    Removed util demo material

--- src/main.c
+++ src/main.c
@@ -25,51 +25,6 @@ int main(void)
     HuffmanCompression(*test);
     printf("\n");
 
-
-    /* LinkedList Demo */
-
-    int i = 150;
-    LinkedList *list = newLinkedList(&i);
-    int j = 200;
-    linkedListAppend(list, &j);
-    int k = 250;
-    linkedListAppend(list, &k);
-    int l = 300;
-    linkedListAppend(list, &l);
-
-    printLinkedList(list, printInt);
-    freeLinkedList(list, NULL);
-
-    /* Account Demo + LinkedList */
-
-    Account *acc = newAccount(readString("Enter a name> "), readString("Enter a URL> "), readString("Enter a password> "));
-
-    Account *acc2 = newAccount(newString("Amazon"), newString("www.amazon.com"), newString("pass123"));
-
-    LinkedList *list2 = newLinkedList(acc);
-    linkedListAppend(list2, acc2);
-
-    printLinkedList(list2, printAccount);
-    freeLinkedList(list2, freeAccount);
-
-
-    /* String Demo */
-
-    String *string = newString(NULL); /* empty String */
-    stringAppend(string, "Appending to the string");
-    printString(string);
-
-    String *string2 = newString("Setting the string at initialisation");
-    printString(string2);
-
-    String *stdRead = readString("Enter a string> "); /* A string can be created directly from stdin */
-    printString(stdRead);
-
-    freeString(string);
-    freeString(string2);
-    freeString(stdRead);
-
-    return 0;
 }
 
 /* Rough Main Menu idea */
