27bb55a6c2039e0e0511023cb354f9d502c3e84d
Joshua Gonzalez <joshgonzalez246@gmail.com>
2021-05-17 15:38:35 +1000

    Merge error fix

--- src/util.h
+++ src/util.h
@@ -226,5 +226,12 @@ typedef char byte;
 #define clearBit(byte, bitToSet) byte &= ~(1 << (bitToSet-1))
 /*void clearBit(byte *byte, int bitToSet); */
 
+/*******************************************************************************
+* Author: Joshua Gonzalez
+* Macro: Checks status of bit (Goes from 8 - 1)
 * Input: ch - byte to have bit checked
+*        pos - position of bit to check
+*******************************************************************************/
+int checkBit(byte ch, int pos);
+
 #endif /* UTIL_H */
