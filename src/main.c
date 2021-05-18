6ea60d2658c6c0e06453b39c3c8b596704c4b900
Peter de Vroom <55121683+OrigamiDev-Pete@users.noreply.github.com>
2021-05-18 12:33:10 +1000

    Reformatted main.c
    Due to quirks in C89 some errors were occurring within some cases. By adding braces for each case these errors won't appear.

--- src/main.c
+++ src/main.c
@@ -8,101 +8,134 @@
 #include "encryption.h"
 
 /* Function Prototypes */
-void printLogin();
-void printMenu();
-void printSearch();
-
+void printLogin(void);
+void printMenu(void);
+void printSearch(void);
 
 int main(void)
 {
+/*   Account *acc = newAccount(newString("Test"), newString("Test.com"), newString("Pass")); */
   LinkedList* accounts = newLinkedList(NULL);
   boolean running = true;
-  while(running==true)
+
+  while(running == true) 
   {
     printLogin();
     int option;
     printf("Option>");
     option = readInt();
-    while(option!=4)
-    {
-      switch(option)
-    {
-      case 1: /* Login (Existing User) */
 
-        printMenu();
-        int task;
-        printf("Option>");
-        task = readInt();
-        while(task!=7)
+    while(option != 4)
+    {
+      switch (option) 
+      {
+        case 1: /* Login (Existing User) */
         {
-          switch(task)
+          int task;
+
+          while(task != 7) /* NOTE(pete): Moved while statement */
           {
-            case 1: /* Search and Sort menu */
+            printMenu();
+            printf("Option>");
+            task = readInt();
 
-              printSearch();
-              int choice;
-              printf("Option>");
-              choice = readInt();
-              while(choice!=5)
+            switch (task)
+            {
+              case 1: /* Search and Sort menu */
               {
-                switch(choice)
+                printSearch();
+                int choice;
+                printf("Option>");
+                choice = readInt();
+
+                while(choice!=5)
                 {
-                  case 1: /* Search database */
-                  break;
-                  case 2: /* Sort database alphabetically */
-                  break;
-                  case 3: /* Edit account entry */
-                  break;
-                  case 4: /*  Delete account */
-                    /* Enter master password*/
-                    /*String* password = readString("Enter password> ");*/
-                  break;
+                  switch(choice)
+                  {
+                    case 1: /* Search database */
+                    { 
+                      break;
+                    }
+                    case 2: /* Sort database alphabetically */
+                    {
+                      break;
+                    }
+                    case 3: /* Edit account entry */
+                    {
+                      break;
+                    }
+                    case 4: /*  Delete account */
+                    {
+                      /* Enter master password*/
+                      /*String* password = readString("Enter password> ");*/
+                      break;
+                    }
+                  }
                 }
-              } 
-
-            break;
-            case 2: /* Add New account */
-              ; /* fix later */
-              String* name = readString("Please enter website's name> ");
-              String* url = readString("Please enter website's url> ");
-              String* password = readString("Please enter password> ");
-              
-              Account* inputAccount = newAccount(name, url, password);
-              linkedListAppend(accounts, inputAccount); 
-            break;
-            case 3: /* Display stored websites */
-
-              printLinkedList(accounts, printAccount);
-            break;
-            case 4: /* Import database (DC&DE) */
-            break;
-            case 5: /* Export database (E&C) */
-            break;
-            case 6: /* Log out */
-              running = false;
-            break;
-            default: printf("Invalid option.\n");
+                break;
+              }
+              case 2: /* Add New account */
+              {
+                String* name = readString("Please enter website's name> ");
+                String* url = readString("Please enter website's url> ");
+                String* password = readString("Please enter password> ");
+                
+                Account* inputAccount = newAccount(name, url, password);
+                linkedListAppend(accounts, inputAccount); 
+                break;
+              }
+              case 3: /* Display stored websites */
+              {
+                printAccountList(accounts);
+                #ifdef DEBUG
+                printLinkedList(accounts, printAccount);
+                #endif
+                break;
+              }
+              case 4: /* Import database (DC&DE) */
+              {
+                break;
+              }
+              case 5: /* Export database (E&C) */
+              {
+                break;
+              }
+              case 6: /* Log out */
+              {
+                running = false;
+                break;
+              }
+              default:
+              {
+                printf("Invalid option.\n");
+                break;
+              }
+            }
           }
+          break;
         }
-
-      break;
-      case 2: /*  Create New Master Account */
-      break;
-      case 3:  /* Exit Program */
-        running =false;
-      break;
-      default: printf("Invalid option.\n");
+        case 2:
+        { /*  Create New Master Account */
+          break;
+        }
+        case 3: 
+        { /* Exit Program */
+          running = false;
+          break;
+        }
+        default:
+        {
+          printf("Invalid option.\n");
+          break;
+        }
+      }
     }
-
   }
-
-  }
-
   return 0;
 }
 
 /* Rough Main Menu idea */
-void printLogin()
+void printLogin(void)
 {
   printf("\n\n"
   "*************************************\n"
@@ -113,7 +146,7 @@ void printLogin()
   "3. Exit Program\n");
 }
 
-void printMenu()
+void printMenu(void)
 {
   printf("\n\n"
   "*************************************\n"
@@ -121,13 +154,13 @@ void printMenu()
   "*************************************\n"
   "1. Search and Sort menu\n"
   "2. Add New account\n"
-  "3. Display stored websites\n"
-  "4. Import database (DC&DE)\n"
+  "3. Display stored websites\n" /* NOTE(pete): Maybe 'Display stored accounts' would be more consistent */
+  "4. Import database (DC&DE)\n" /* NOTE(pete): Maybe Save/Load makes more sense than Import Export. Thoughts on Auto-Saving? */ 
   "5. Export database (E&C)\n"
   "6. Log out\n");
 }
 
-void printSearch()
+void printSearch(void)
 {
   printf("\n\n"
   "*************************************\n"
